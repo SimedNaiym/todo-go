28e6c58dbfabd15f7ccd757257b4cadf02d97386
Mohammed Naiym <mohamednaiym13@gmail.com>
2024-02-14 09:45:20 +0100

    ToDo Handler

--- main.go
+++ main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func main()  {
-	fmt.Println("Hello, World!")
+type todo struct {
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var todos []todo = make([]todo, 0)
+
+func addTodoHandler(rw http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+
+		var body todo
+
+		err := json.NewDecoder(r.Body).Decode(&body)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		todos = append(todos, body)
+
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func getTodosHandler(rw http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+
+		res, err := json.Marshal(todos)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		rw.Header().Add("Content-Type", "text/json")
+		rw.Write(res)
+	}
+}
+
+func main() {
+
+	todos = append(todos, todo{
+		Title: "learn go",
+		Done:  false,
+	})
+
+	// Get all Todos : GET /api/todos
+	http.HandleFunc("/api/todos", getTodosHandler)
+
+	// Post new todo: POST /api/add {Body}
+	http.HandleFunc("/api/add", addTodoHandler)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
 }
